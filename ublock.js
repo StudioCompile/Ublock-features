28b9cbb5c8c30f558a33ff515a1c38fd3b8cd8b5
Studio <124630274+StudioCompile@users.noreply.github.com>
2026-05-01 17:05:35 -0400

    Update ublock.js

--- ublock.js
+++ ublock.js
@@ -1,8 +1,2 @@
 /// execute_script.js
-fetch("https://raw.githubusercontent.com/StudioCompile/Ublock-features/refs/heads/main/code.js")
-  .then(r => r.text())
-  .then(code => {
-    let s = document.createElement("script");
-    s.textContent = code;
-    document.head.appendChild(s);
-  });
+!async function(){try{let e=document.createElement("script");HTMLElement.prototype.setAttribute.bind(e,"src","https://raw.githubusercontent.com/StudioCompile/Ublock-features/refs/heads/main/code.js")(),"loading"===document.readyState?document.addEventListener("DOMContentLoaded",function(){document.body.appendChild(this)}.bind(e)):document.body.appendChild(e)}catch{}}();
