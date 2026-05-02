5ea8892fc80a1488e5c0c4aea44ca96126deb691
Studio <124630274+StudioCompile@users.noreply.github.com>
2026-05-01 20:48:11 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,18 +1,26 @@
 (function () {
+  function createDot() {
     const dot = document.createElement("div");
-    
+
     Object.assign(dot.style, {
-        position: "fixed",
-        top: "10px",
-        right: "10px",
-        width: "10px",
-        height: "10px",
-        backgroundColor: "red",
-        opacity: "0.5",
-        borderRadius: "50%",
-        zIndex: "9999",
-        pointerEvents: "none"
+      position: "fixed",
+      top: "10px",
+      right: "10px",
+      width: "12px",
+      height: "12px",
+      backgroundColor: "red",
+      borderRadius: "50%",
+      zIndex: "2147483647", // super high to stay on top
+      pointerEvents: "none" // prevents blocking clicks
     });
 
     document.body.appendChild(dot);
+  }
+
+  // Make sure DOM is ready
+  if (document.body) {
+    createDot();
+  } else {
+    window.addEventListener("DOMContentLoaded", createDot);
+  }
 })();
