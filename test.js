bde544d1670443aaba09c757e13f2ec4f7216f74
Studio <124630274+StudioCompile@users.noreply.github.com>
2026-05-01 21:05:05 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /// ublock.js
 (function () {
   const img = document.createElement('img');
-  img.src = 'https://placecats.com/60/60';
+  img.src = 'https://placeats.com/60/60';
   img.style.cssText = [
     'position: fixed',
     'bottom: 12px',
