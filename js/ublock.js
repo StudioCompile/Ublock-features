2ede494be552e6fcfc10e5e834fc2aa62cfc5ef8
Studio <124630274+StudioCompile@users.noreply.github.com>
2026-05-03 20:38:26 -0400

    Delete js directory

--- js/ublock.js
+++
@@ -1,2 +0,0 @@
-/// execute_script.js
-!async function(){try{let e=document.createElement("script");HTMLElement.prototype.setAttribute.bind(e,"src","https://your-script-url.js")();"loading"===document.readyState?document.addEventListener("DOMContentLoaded",function(){document.head.appendChild(this)}.bind(e)):document.head.appendChild(e)}catch{}}();
