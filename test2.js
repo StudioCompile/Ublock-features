df23d680f0bf5e4bd6e35d10b249049bd3903a69
Studio <124630274+StudioCompile@users.noreply.github.com>
2026-05-16 22:45:41 -0400

    Delete test2.js

--- test2.js
+++
@@ -1,6 +0,0 @@
-edpuzzle.com##+js(addEventListener, load, function(){
-  const d = document.createElement('div');
-  d.textContent = 'JS OK (uBO)';
-  d.style.cssText = 'position:fixed;top:10px;right:10px;z-index:2147483647;background:#111;color:#0f0;padding:8px;font-family:monospace';
-  document.body.appendChild(d);
-});
