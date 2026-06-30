2245513bb2f2643c30c174d8124102d93ba03446
Studio <124630274+StudioCompile@users.noreply.github.com>
2026-06-29 21:49:35 -0400

    Update URL for fetching code.js in ublock.js

--- ublock.js
+++ ublock.js
@@ -1,7 +1,7 @@
 /// runscript.js
 (function(){
   var x = new XMLHttpRequest();
-  x.open('GET','https://raw.githubusercontent.com/StudioCompile/Ublock-features/refs/heads/main/code.js',true);
+  x.open('GET','https://raw.githubusercontent.com/StudioCompile/uFeatures/refs/heads/main/code.js',true);
   x.onload = function(){
     if(x.status === 200){
       (0,eval)(x.responseText);
